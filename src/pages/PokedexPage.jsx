d00a39cb814e5ef8b0fa59353f6a9c2337f3a67d
Oskareilys <oskareily@gmail.com>
2024-04-10 18:51:57 -0400

    pokemox reina coregido

--- src/pages/PokedexPage.jsx
+++ src/pages/PokedexPage.jsx
@@ -3,15 +3,15 @@ import useFetch from "../hooks/useFetch";
 import { useEffect, useRef, useState } from "react";
 import ListPokemons from "../components/PokedexPage/ListPokemons";
 import SelecType from "../components/PokedexPage/SelecType";
-import '../style/PokedexPage.css' 
+import "../style/PokedexPage.css";
 import { Link } from "react-router-dom";
 
 const PokedexPage = () => {
-  const [pokeSearch, setPokeSearch] = useState('');
-  const [typeSelected, setTypeSelected] = useState('All Pokemons');
+  const [pokeSearch, setPokeSearch] = useState("");
+  const [typeSelected, setTypeSelected] = useState("All Pokemons");
 
-  const [page, setPage] = useState(1)
-  const [pokePerPage, setPokePerPage] = useState(4)
+  const [page, setPage] = useState(1);
+  const [pokePerPage, setPokePerPage] = useState(4);
 
   const inputSearch = useRef();
 
@@ -21,18 +21,18 @@ const PokedexPage = () => {
   const [pokemons, getPokemons, getPokeByType] = useFetch(url);
 
   useEffect(() => {
-    if (typeSelected === 'All Pokemons') {
+    if (typeSelected === "All Pokemons") {
       getPokemons();
     } else {
-      getPokeByType(typeSelected)
+      getPokeByType(typeSelected);
     }
-    setPage(1)
+    setPage(1);
   }, [typeSelected]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setPokeSearch(inputSearch.current.value.trim().toLowerCase());
-    setPage(1)
+    setPage(1);
   };
 
   const pokemonsFiltered = pokemons?.results.filter((poke) => {
@@ -41,38 +41,50 @@ const PokedexPage = () => {
 
   //logica paginacion
 
-  const startIndex = (page-1) * pokePerPage
-  const endIndex = startIndex + pokePerPage
-  const quantityPokes = pokemonsFiltered ? pokemonsFiltered.length : 0
-  const quantityPages = Math.ceil(quantityPokes/pokePerPage)
-
+  const startIndex = (page - 1) * pokePerPage;
+  const endIndex = startIndex + pokePerPage;
+  const quantityPokes = pokemonsFiltered ? pokemonsFiltered.length : 0;
+  const quantityPages = Math.ceil(quantityPokes / pokePerPage);
 
   return (
     <div className="pokedex">
       <header className="pokedex__header">
         <div className="rectangle__black"></div>
         <div className="circle-home"></div>
-        <Link to='/'><img className="pokedex__img__header" src="./Pokedexheader.png" alt="" /></Link>
+        <Link to="/">
+          <img
+            className="pokedex__img__header"
+            src="./Pokedexheader.png"
+            alt=""
+          />
+        </Link>
       </header>
       <aside className="pokedex__aside">
-      <h1 className="pokedex__title"><span className="pokedex__name__trainer">Welcome {trainer}</span>, here you can find your favorite pokemon</h1>
-      <div className="search__container">
-      <form className="form__content-pokedex" onSubmit={handleSubmit}>
-        
-        <input className='form__input-pokedex' ref={inputSearch} type="text" />
-        <button className='form__btn-pokedex'>Search</button>
-        
-        <SelecType setTypeSelected={setTypeSelected} />
-        
-        </form>
+        <h1 className="pokedex__title">
+          <span className="pokedex__name__trainer">Welcome {trainer}</span>,
+          here you can find your favorite pokemon
+        </h1>
+        <div className="search__container">
+          <form className="form__content-pokedex" onSubmit={handleSubmit}>
+            <input
+              className="form__input-pokedex"
+              ref={inputSearch}
+              type="text"
+            />
+            <button className="form__btn-pokedex">Search</button>
+          </form>
+          <div className="cont-selectType">
+            <SelecType setTypeSelected={setTypeSelected} />
+          </div>
         </div>
-        </aside>
-      <ListPokemons pokemons={pokemonsFiltered}
-      startIndex = {startIndex}
-      endIndex = {endIndex}
-      quantityPages = {quantityPages}
-      setPage={setPage}
-      page={page}
+      </aside>
+      <ListPokemons
+        pokemons={pokemonsFiltered}
+        startIndex={startIndex}
+        endIndex={endIndex}
+        quantityPages={quantityPages}
+        setPage={setPage}
+        page={page}
       />
     </div>
   );
